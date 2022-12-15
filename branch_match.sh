621f1c990d2e88f1abd8bce22e0dbf602aa7c134
Tom Janoski <tj@tomjanoski.com>
2022-12-15 14:26:11 -0600

    fixed stupid branch var

--- branch_match.sh
+++ branch_match.sh
@@ -1,10 +1,9 @@
 
 string="chore/pss-1-example"
-# regx='/\b(feat|bug|chore)\b\/\b(pss|ui)\b-\b[1-9][0-9]{0,4}\b.*'
 branch_pattern_regx='refs\/heads\/(feat|bug|chore)\/(pss|ui)-[0-9]{0,4}.*'
 stable_branch_pattern='refs\/heads\/(main|dev|test)'
 
-if [[ $BRANCH_NAME =~ $regx ]]; then
+if [[ $BRANCH_NAME =~ $branch_pattern_regx ]]; then
         echo "Branch name matched!"
     elif [[ $BRANCH_NAME =~ $stable_branch_pattern ]]; then
         echo "Stable Branch detected!"
