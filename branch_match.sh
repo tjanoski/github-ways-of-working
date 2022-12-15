8e64aa9205fd21557918df8d93d9eb83674f0c89
Tom Janoski <tj@tomjanoski.com>
2022-12-15 13:50:29 -0600

    fixed for main

--- branch_match.sh
+++ branch_match.sh
@@ -1,10 +1,13 @@
 
 string="chore/pss-1-example"
 # regx='/\b(feat|bug|chore)\b\/\b(pss|ui)\b-\b[1-9][0-9]{0,4}\b.*'
-regx='(feat|bug|chore)\/(pss|ui)-[0-9]{0,4}.*'
+branch_pattern_regx='(feat|bug|chore)\/(pss|ui)-[0-9]{0,4}.*'
+stable_branch_pattern='(main|dev|test)'
 
 if [[ ${{steps.extract_branch.outputs.branch}} =~ $regx ]]; then
         echo "Branch name matched!"
-        else
+    elif [[ ${{steps.extract_branch.outputs.branch}} =~ $stable_branch_pattern ]]; then
+        echo "Stable Branch detected!"
+    else
         exit 1
     fi
