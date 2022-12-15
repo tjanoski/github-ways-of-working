28aa8376698a7cebded9acaa723e30a756c541ae
Tom Janoski <tj@tomjanoski.com>
2022-12-15 14:25:17 -0600

    fix?

--- branch_match.sh
+++ branch_match.sh
@@ -4,7 +4,7 @@ string="chore/pss-1-example"
 branch_pattern_regx='refs\/heads\/(feat|bug|chore)\/(pss|ui)-[0-9]'
 stable_branch_pattern='refs\/heads\/(main|dev|test)'
 
-if [[ $BRANCH_NAME =~ $regx ]]; then
+if [[ $BRANCH_NAME =~ $branch_pattern_regx ]]; then
         echo "Branch name matched!"
     elif [[ $BRANCH_NAME =~ $stable_branch_pattern ]]; then
         echo "Stable Branch detected!"
